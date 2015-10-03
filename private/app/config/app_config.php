d29b9b27cb88d56e288075583c420d573ee1201e
= <=>
2015-10-03 16:47:14 +0545

    Cleaned app_config.php.

--- private/app/config/app_config.php
+++ private/app/config/app_config.php
@@ -1,37 +1,28 @@
 <?php
 
 class app_config {
-	
-	public static $url = array(
-		# Currently rewrite true is required for other $_GET[] which comes after $_GET['q']
-		'rewrite'	=> "false",
-		'suffix'		=> ".html"
-	);
-		
-	public static $router = array(
-		'controller'	=> "home",
-		'action'		=> "index"
-	);
-	
-	public static $re_route = array(
-		'admin'		=> array(
-			"users",
-			"account_types",
-			"customers",
-			"accounts",
-			"transactions",
-			"profile",
-		)
-	);
-	
-	public static $db = array(
-		#'host'		=> 'YOUR_DB_HOSTNAME',
-		#'user'		=> 'YOUR_DB_USERNAME',
-		#'pass'		=> 'YOUR_DB_PASSWORD',
-		#'name'		=> 'YOUR_DB_NAME',
-		#'tpfx'		=> 'YOUR_DB_TABLE_PREFIX'
-	);
-	
-	public static $use_db = false;
-		
+
+    public static $url = array(
+        # Currently rewrite true is required for other $_GET[] which comes after $_GET['q']
+        'rewrite' => "false",
+        'suffix'  => "",
+    );
+
+    public static $router = array(
+        'controller' => "home",
+        'action'     => "index",
+    );
+
+    public static $re_route = array();
+
+    public static $db = array(
+        #'host'        => 'YOUR_DB_HOSTNAME',
+        #'user'        => 'YOUR_DB_USERNAME',
+        #'pass'        => 'YOUR_DB_PASSWORD',
+        #'name'        => 'YOUR_DB_NAME',
+        #'tpfx'        => 'YOUR_DB_TABLE_PREFIX'
+    );
+
+    public static $use_db = false;
+
 }
